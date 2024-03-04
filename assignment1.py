772a8298ff9c477d92c92cb30f3e6655b917bd46
Yash2402 <mrsmeenakumaris@gmail.com>
2024-03-04 22:31:05 +0530

    modified:   assignment1.py 	modified:   assignment2.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,5 @@
 print("Hello World!")
+import math
+
+radius = int(input("Enter the radius of the circle: "))
+print(math.pi * radius * radius)
--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,1 @@
 print("Yash sharma")
-import math
-
-radius = int(input("Enter the radius of the circle: "))
-print(math.pi * radius * radius)
